a7048b5ce3cb404cadfd88e61c4e64ae78ea7ddb
Leela Anuroop A <38076999+leelaakula@users.noreply.github.com>
2021-03-27 20:46:36 +0530

    integration-actions

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -27,7 +27,7 @@ class CoursesPage extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <h2>Courses</h2>
-        <h3>Add Cours</h3>
+        <h3>Add Courses</h3>
         <input type='text' onChange={this.handleChange} value={this.state.course.title} />
         <input type='submit' value='save' />
         {this.props.courses.map((course) => {
