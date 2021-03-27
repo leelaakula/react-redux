3752772add1a74222ef32cbd30e27e2744acc903
Leela Anuroop A <38076999+leelaakula@users.noreply.github.com>
2021-03-27 17:48:00 +0530

    basic form submission

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: ''
+    }
+  };
+
+  // this.handleChange = this.handleChange.bind(this);
+
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
+
   render() {
-    return <h2>Courses</h2>
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Cours</h3>
+        <input type='text' onChange={this.handleChange} value={this.state.course.title} />
+        <input type='submit' value='save' />
+      </form>
+    );
   }
 }
 
