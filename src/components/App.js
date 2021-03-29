9966e625eb319ecde12642a7ccc8c91752db13dd
Leela Anuroop A <38076999+leelaakula@users.noreply.github.com>
2021-03-29 12:51:56 +0530

    class manageCoursPage added

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import AboutPage from './about/AboutPage';
 import Header from './common/Header';
 import CoursesPage from './courses/CoursesPage';
+import ManageCoursePage from './courses/ManageCoursePage';
 import HomePage from './home/HomePage';
 import PageNotFound from './PageNotFound';
 
@@ -14,10 +15,12 @@ const App = () => {
         <Route exact path='/' component={HomePage} />
         <Route path='/about' component={AboutPage} />
         <Route path='/courses' component={CoursesPage} />
+        <Route path='/courses/:slug' component={ManageCoursePage} />
+        <Route path='/course' component={ManageCoursePage} />
         <Route component={PageNotFound} />
       </Switch>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { loadCourses } from '../../redux/actions/courseActions';
+import { loadAuthors } from '../../redux/actions/authorActions';
+import PropTypes from 'prop-types';
+
+class ManageCoursePage extends React.Component {
+  componentDidMount() {
+    const { courses, authors, loadCourses, loadAuthors } = this.props;
+
+    if (courses.length === 0) {
+      loadCourses().catch((err) => {
+        alert('courses failed' + err);
+      });
+    }
+    if (authors.length === 0) {
+      loadAuthors().catch((err) => {
+        alert('authors failed' + err);
+      });
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h2>Manage Course</h2>
+      </>
+    );
+  }
+}
+
+ManageCoursePage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+    authors: state.authors
+  };
+}
+
+const mapDispatchToProps = {
+  loadCourses,
+  loadAuthors
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
+// const mapDispatchToProps = {
+//   loadCourses: courseActions.loadCourses,
+//   loadAuthors: authorActions.loadAuthors
+// };
+
+// const mapDispatchToProps = {
+//   createCourse: courseActions.createCourse
+// };
+
+// const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps);
+// export default connectedStateAndProps(ManageCoursePage);
